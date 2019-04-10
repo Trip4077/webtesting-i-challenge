a9d533087f421b093a280085454c87214edcaefe
Trip4077 <bj2336@email.vccs.edu>
2019-04-10 08:51:31 -0400

    Repair Unit Test Fails

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,18 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe('Enhancer', () => {
+
+    describe('repair()', () => {
+        
+        it('should return the passed item with 100 durability', () => {
+            //Arrange
+
+            //Assert
+
+            //Act
+
+            return null;
+        });
+
+    })
+})
