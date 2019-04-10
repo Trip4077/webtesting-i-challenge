51d83439eb452e1a0dc95c3b898e7151c466c701
Trip4077 <bj2336@email.vccs.edu>
2019-04-10 10:07:17 -0400

    Succeed Unit Test Fails

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,4 +1,4 @@
-const { repair } = require('./enhancer.js');
+const { repair, succeed } = require('./enhancer.js');
 const items = require('../items');
 
 // test away!
@@ -27,7 +27,7 @@ describe('Enhancer', () => {
 
             expect(repair(nullDurability)).toEqual(nullDurability);
             expect(repair(undefinedDurability)).toEqual(undefinedDurability);
-        })
+        });
 
         it('should convert a string to number if conversion possible else throw error', () => {
             const convertable = items[4];
@@ -37,9 +37,38 @@ describe('Enhancer', () => {
                 ...convertable,
                 durability: 100
             }
-            
+
             expect(repair(convertable)).toEqual(expectedConvert);
             expect(() => { repair(nonConvertable) }).toThrow();
-        })
+        });
+    });
+
+    describe('succeed()', () => {
+
+        it('should increase enhancment level by 1', () => {
+            const item1 = items[0];
+            const item2 = items[1];
+
+            const expected1 = {
+                ...item1,
+                enhancement: item1.enhancement + 1
+            }
+
+            const expected2 = {
+                ...item2,
+                enhancement: item2.enhancement + 1
+            }
+
+            expect(succeed(item1)).toEqual(expected1);
+            expect(succeed(item2)).toEqual(expected2);
+        });
+
+        it.todo('should not increment if enhancement is 20');
+
+        it.todo('should not change durability');
+
+        it.todo('should return the item unchanged if enhancement is null or undefined');
+
+        it.todo('should convert a string to number if conversion possible else throw error');
     })
 })
